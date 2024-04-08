cb5a8e95f092646fb91ba355b150a9ceee14c512
Satyabrata69 <158498028+Satyabrata69@users.noreply.github.com>
2024-04-08 23:45:32 +0530

    Add files via upload

---
+++ interface1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+interface code{
+    void input();
+    void output();
+}
+class satya implements code{
+    String name; double sal;
+    public void input(){
+        try (Scanner ac = new Scanner(System.in);) {
+            System.out.println("enter the name:");
+            name=ac.nextLine();
+            System.out.println("enter the salary:");
+            sal=ac.nextDouble();
+        }
+    }    
+    public void output(){
+    System.out.println(name+" "+sal);
+    }    
+}
+public class interface1 {
+    public static void main(String args[])
+    {
+        code r=new satya();
+        r.input();
+        r.output();    
+    }
+}
